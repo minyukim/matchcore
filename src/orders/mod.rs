99720d18c47a709b99257ab10929cba240d8d11e
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-12 11:38:58 +0900

    refactor: rename time_in_force module to time

--- src/orders/mod.rs
+++ src/orders/mod.rs
@@ -1,11 +1,11 @@
 mod peg;
 mod qty;
 mod side;
-mod time_in_force;
+mod time;
 
 mod tests;
 
 pub use peg::PegReference;
 pub use qty::QtyPolicy;
 pub use side::Side;
-pub use time_in_force::TimeInForce;
+pub use time::TimeInForce;
