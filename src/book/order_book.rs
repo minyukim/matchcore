a4167b0230fafcea2842aab04d5770ea5dcfc7ce
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-19 11:36:06 +0900

    feat: impl basic methods

--- src/book/order_book.rs
+++ src/book/order_book.rs
@@ -40,3 +40,54 @@ where
     /// Pegged orders indexed by order ID for O(1) lookup
     pegged_orders: HashMap<u64, PeggedOrder<E>>,
 }
+
+impl<E: Clone + Copy + Eq + Serialize + core::fmt::Debug> OrderBook<E> {
+    /// Create a new order book
+    pub fn new(symbol: String) -> Self {
+        Self {
+            symbol,
+            bids: BTreeMap::new(),
+            asks: BTreeMap::new(),
+            orders: HashMap::new(),
+            pegged_bids: core::array::from_fn(|_| PegLevel::new()),
+            pegged_asks: core::array::from_fn(|_| PegLevel::new()),
+            pegged_orders: HashMap::new(),
+            last_trade_price: None,
+        }
+    }
+
+    pub fn last_trade_price(&self) -> Option<u64> {
+        self.last_trade_price
+    }
+
+    /// Get the symbol for this order book
+    pub fn symbol(&self) -> &str {
+        &self.symbol
+    }
+
+    /// Get the best bid price, if any
+    /// O(1) operation using the last key (highest price) in the BTreeMap
+    pub fn best_bid(&self) -> Option<u64> {
+        self.bids.keys().next_back().copied()
+    }
+
+    /// Get the best ask price, if any
+    /// O(1) operation using the first key (lowest price) in the BTreeMap
+    pub fn best_ask(&self) -> Option<u64> {
+        self.asks.keys().next().copied()
+    }
+
+    /// Get the mid price (average of best bid and best ask)
+    pub fn mid_price(&self) -> Option<f64> {
+        let best_bid = self.best_bid()?;
+        let best_ask = self.best_ask()?;
+        Some((best_bid as f64 + best_ask as f64) / 2.0)
+    }
+
+    /// Get the spread (difference between best bid and best ask)
+    pub fn spread(&self) -> Option<u64> {
+        let best_bid = self.best_bid()?;
+        let best_ask = self.best_ask()?;
+        Some(best_ask - best_bid)
+    }
+}
