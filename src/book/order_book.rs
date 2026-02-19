44ea1b90bd3544fe9a79af939a3bd860a664f0a0
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-19 18:15:35 +0900

    feat: add last sequence and timestamp

--- src/book/order_book.rs
+++ src/book/order_book.rs
@@ -19,6 +19,14 @@ where
     /// The symbol for this order book
     symbol: String,
 
+    /// The last sequence number of the order book
+    /// This is used to ensure that the incoming orders are processed in the correct order.
+    last_sequence_number: u64,
+
+    /// The last processed timestamp of the order book, expressed as a Unix timestamp (seconds since epoch).
+    /// This is used to ensure that the timestamps of the incoming orders are non-decreasing.
+    last_processed_timestamp: u64,
+
     /// The last price at which a trade occurred, `None` if no trade has occurred yet
     last_trade_price: Option<u64>,
 
@@ -46,14 +54,26 @@ impl<E: Clone + Copy + Eq + Serialize + core::fmt::Debug> OrderBook<E> {
     pub fn new(symbol: String) -> Self {
         Self {
             symbol,
+            last_sequence_number: 0,
+            last_processed_timestamp: 0,
+            last_trade_price: None,
             bids: BTreeMap::new(),
             asks: BTreeMap::new(),
             orders: HashMap::new(),
             pegged_bids: core::array::from_fn(|_| PegLevel::new()),
             pegged_asks: core::array::from_fn(|_| PegLevel::new()),
             pegged_orders: HashMap::new(),
-            last_trade_price: None,
         }
+    }
+
+    /// Get the last sequence number of the order book
+    pub fn last_sequence_number(&self) -> u64 {
+        self.last_sequence_number
+    }
+
+    /// Get the last processed timestamp of the order book
+    pub fn last_processed_timestamp(&self) -> u64 {
+        self.last_processed_timestamp
     }
 
     pub fn last_trade_price(&self) -> Option<u64> {
