9b3832ec66754b8151dcf021a44fbcc94f1a5987
Minhyuk Kim <105956521+minyukim@users.noreply.github.com>
2026-02-23 21:35:04 +0900

    Define execution report (#30)
    feat: define execution report

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -1,5 +1,6 @@
 mod cancel_reason;
 mod command;
+mod execution_report;
 mod match_result;
 mod order_processing_result;
 mod trade;
@@ -8,6 +9,7 @@ mod tests;
 
 pub use cancel_reason::CancelReason;
 pub use command::Command;
+pub use execution_report::ExecutionReport;
 pub use match_result::MatchResult;
 pub use order_processing_result::OrderProcessingResult;
 pub use trade::Trade;
