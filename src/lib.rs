f644107be3bf3dd1bc66dbc0cd5ea86d6e6319bd
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-23 21:33:06 +0900

    feat: define execution report

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ mod execution;
 mod order;
 
 pub use book::{OrderBook, PegLevel, PriceLevel};
-pub use execution::{CancelReason, Command, MatchResult, OrderProcessingResult, Trade};
+pub use execution::{
+    CancelReason, Command, ExecutionReport, MatchResult, OrderProcessingResult, Trade,
+};
 pub use order::{
     LimitOrder, OrderCore, OrderType, PegReference, PeggedOrder, QuantityPolicy, Side, TimeInForce,
 };
