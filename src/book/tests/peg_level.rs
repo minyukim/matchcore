cebb0f6d84debd6980ee1cdc5cde04c71065dee9
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-23 12:39:28 +0900

    feat(peglevel): add methods for matching; update visibility

--- src/book/tests/peg_level.rs
+++ src/book/tests/peg_level.rs
@@ -3,31 +3,14 @@ mod tests_peg_level {
     use crate::book::PegLevel;
 
     #[test]
-    fn test_push_and_peek() {
+    fn test_order_count() {
         let mut peg_level = PegLevel::new();
-        assert_eq!(peg_level.peek(), None);
+        assert_eq!(peg_level.order_count(), 0);
 
-        peg_level.push(1);
-        assert_eq!(peg_level.peek(), Some(1));
-        assert_eq!(peg_level.peek(), Some(1));
+        peg_level.increment_order_count();
+        assert_eq!(peg_level.order_count(), 1);
 
-        peg_level.push(2);
-        assert_eq!(peg_level.peek(), Some(1));
-    }
-
-    #[test]
-    fn test_push_and_pop() {
-        let mut peg_level = PegLevel::new();
-        assert_eq!(peg_level.pop(), None);
-
-        peg_level.push(1);
-        assert_eq!(peg_level.pop(), Some(1));
-        assert_eq!(peg_level.pop(), None);
-
-        peg_level.push(2);
-        peg_level.push(3);
-        assert_eq!(peg_level.pop(), Some(2));
-        assert_eq!(peg_level.pop(), Some(3));
-        assert_eq!(peg_level.pop(), None);
+        peg_level.decrement_order_count();
+        assert_eq!(peg_level.order_count(), 0);
     }
 }
