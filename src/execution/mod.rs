03fff67225ff1fd05adb4cf8e7d413eabde7800b
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-24 11:14:52 +0900

    refactor: separate commands

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -1,15 +1,13 @@
 mod cancel_reason;
-mod command;
-mod execution_report;
 mod match_result;
 mod order_processing_result;
+mod report;
 mod trade;
 
 mod tests;
 
 pub use cancel_reason::CancelReason;
-pub use command::Command;
-pub use execution_report::ExecutionReport;
 pub use match_result::MatchResult;
 pub use order_processing_result::OrderProcessingResult;
+pub use report::ExecutionReport;
 pub use trade::Trade;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 mod book;
+mod commands;
 mod execution;
 mod orders;
 mod types;
 
 pub use book::{OrderBook, PegLevel, PriceLevel};
-pub use execution::{
-    CancelReason, Command, ExecutionReport, MatchResult, OrderProcessingResult, Trade,
-};
+pub use commands::*;
+pub use execution::{CancelReason, ExecutionReport, MatchResult, OrderProcessingResult, Trade};
 pub use orders::*;
 pub use types::*;
