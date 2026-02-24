53f520f91ca476027916923bee29dcf8c4160ce4
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-24 13:32:13 +0900

    feat: define amend logic template

--- src/book/amend.rs
+++ src/book/amend.rs
@@ -9,6 +9,31 @@ impl<E: Clone + Copy + Eq + Serialize + for<'de> Deserialize<'de> + core::fmt::D
         &mut self,
         amend_cmd: AmendCmd,
     ) -> Result<AmendReport, ExecutionError> {
+        match amend_cmd.changes {
+            AmendChanges::Limit(amend) => self.amend_limit_order(amend_cmd.order_id, amend),
+            AmendChanges::Pegged(amend) => self.amend_pegged_order(amend_cmd.order_id, amend),
+        }
+    }
+
+    /// Amend a limit order
+    fn amend_limit_order(
+        &mut self,
+        order_id: u64,
+        amend: LimitAmend,
+    ) -> Result<AmendReport, ExecutionError> {
+        _ = order_id;
+        _ = amend;
+        todo!()
+    }
+
+    /// Amend a pegged order
+    fn amend_pegged_order(
+        &mut self,
+        order_id: u64,
+        amend: PeggedAmend,
+    ) -> Result<AmendReport, ExecutionError> {
+        _ = order_id;
+        _ = amend;
         todo!()
     }
 }
