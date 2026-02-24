937c001c9a74f02a70747dd795bf0505b5962461
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-02-24 13:34:00 +0900

    feat: define cancel logic template

--- src/book/cancel.rs
+++ src/book/cancel.rs
@@ -6,6 +6,21 @@ impl<E: Clone + Copy + Eq + Serialize + for<'de> Deserialize<'de> + core::fmt::D
     /// Execute a cancel command against the order book
     /// Returns the execution report for the command
     pub(super) fn execute_cancel(&mut self, cancel_cmd: CancelCmd) -> Result<(), ExecutionError> {
+        match cancel_cmd.order_kind {
+            OrderKind::Limit => self.cancel_limit_order(cancel_cmd.order_id),
+            OrderKind::Pegged => self.cancel_pegged_order(cancel_cmd.order_id),
+        }
+    }
+
+    /// Cancel a limit order
+    fn cancel_limit_order(&mut self, order_id: u64) -> Result<(), ExecutionError> {
+        _ = order_id;
+        todo!()
+    }
+
+    /// Cancel a pegged order
+    fn cancel_pegged_order(&mut self, order_id: u64) -> Result<(), ExecutionError> {
+        _ = order_id;
         todo!()
     }
 }
