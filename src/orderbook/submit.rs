96329b2f4d9f6bb850ed5a5d4ee8ceedee86ff54
Minhyuk Kim <105956521+minyukim@users.noreply.github.com>
2026-03-02 11:14:40 +0900

    Rename order to spec for order spec type parameters (#53)
    refactor: submit spec

--- src/orderbook/submit.rs
+++ src/orderbook/submit.rs
@@ -11,9 +11,9 @@ impl OrderBook {
     /// Returns the execution report for the command
     pub(super) fn execute_submit(&mut self, meta: CommandMeta, cmd: &SubmitCmd) -> CommandOutcome {
         let result = match &cmd.order {
-            NewOrder::Market(order) => self.submit_market_order(meta, order),
-            NewOrder::Limit(order) => self.submit_limit_order(meta, order),
-            NewOrder::Pegged(order) => self.submit_pegged_order(meta, order),
+            NewOrder::Market(spec) => self.submit_market_order(meta, spec),
+            NewOrder::Limit(spec) => self.submit_limit_order(meta, spec),
+            NewOrder::Pegged(spec) => self.submit_pegged_order(meta, spec),
         };
 
         match result {
@@ -26,20 +26,20 @@ impl OrderBook {
     fn submit_market_order(
         &mut self,
         meta: CommandMeta,
-        order: &MarketOrderSpec,
+        spec: &MarketOrderSpec,
     ) -> Result<SubmitReport, RejectReason> {
-        order.validate().map_err(RejectReason::CommandError)?;
+        spec.validate().map_err(RejectReason::CommandError)?;
 
-        if self.is_side_empty(order.side().opposite()) {
+        if self.is_side_empty(spec.side().opposite()) {
             return Err(RejectReason::NoLiquidity);
         }
 
         let order_id = meta.sequence_number;
 
-        let result = self.match_order(order.side(), None, order.quantity(), meta.timestamp);
+        let result = self.match_order(spec.side(), None, spec.quantity(), meta.timestamp);
 
         let executed_quantity = result.executed_quantity();
-        let remaining_quantity = order.quantity() - executed_quantity;
+        let remaining_quantity = spec.quantity() - executed_quantity;
         if remaining_quantity == 0 {
             return Ok(SubmitReport::new(
                 OrderProcessingResult::new(order_id).with_match_result(result),
@@ -48,7 +48,7 @@ impl OrderBook {
 
         // If the order is a market to limit order and there is a remaining quantity,
         // convert it to a limit order at the last trade price
-        if order.market_to_limit() {
+        if spec.market_to_limit() {
             // The last trade price is guaranteed to exist because the order was matched
             let price = self.last_trade_price.unwrap();
 
@@ -62,19 +62,19 @@ impl OrderBook {
                         QuantityPolicy::Standard {
                             quantity: remaining_quantity,
                         },
-                        OrderFlags::new(order.side(), false, TimeInForce::Gtc),
+                        OrderFlags::new(spec.side(), false, TimeInForce::Gtc),
                     ),
                 ),
             );
 
-            let price_levels = match order.side() {
+            let price_levels = match spec.side() {
                 Side::Buy => &mut self.limit_bid_levels,
                 Side::Sell => &mut self.limit_ask_levels,
             };
             price_levels.insert(price, price_level);
 
             let triggered_orders =
-                self.trigger_opposite_side_takers(order.side().opposite(), meta.timestamp);
+                self.trigger_opposite_side_takers(spec.side().opposite(), meta.timestamp);
 
             return Ok(SubmitReport::new(
                 OrderProcessingResult::new(order_id).with_match_result(result),
@@ -86,7 +86,7 @@ impl OrderBook {
             OrderProcessingResult::new(order_id)
                 .with_match_result(result)
                 .with_cancel_reason(CancelReason::InsufficientLiquidity {
-                    requested_quantity: order.quantity(),
+                    requested_quantity: spec.quantity(),
                     available_quantity: executed_quantity,
                 }),
         ))
@@ -96,7 +96,7 @@ impl OrderBook {
     fn submit_limit_order(
         &mut self,
         _meta: CommandMeta,
-        _order: &LimitOrderSpec,
+        _spec: &LimitOrderSpec,
     ) -> Result<SubmitReport, RejectReason> {
         todo!()
     }
@@ -105,7 +105,7 @@ impl OrderBook {
     fn submit_pegged_order(
         &mut self,
         _meta: CommandMeta,
-        _order: &PeggedOrderSpec,
+        _spec: &PeggedOrderSpec,
     ) -> Result<SubmitReport, RejectReason> {
         todo!()
     }
