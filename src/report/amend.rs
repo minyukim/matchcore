7a90c409730c185e969d35d63292a61c17e923ad
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-08 21:26:48 +0900

    feat: order processing results -> submit result -> amend result

--- src/report/amend.rs
+++ src/report/amend.rs
@@ -1,4 +1,5 @@
-use super::{OrderProcessingResult, OrderProcessingResults};
+use super::{OrderProcessingResult, OrderProcessingResults, SubmitReport};
+use crate::OrderId;
 
 use serde::{Deserialize, Serialize};
 
@@ -7,7 +8,7 @@ use serde::{Deserialize, Serialize};
 pub struct AmendReport {
     /// The new order ID, if the amend command resulted in an order replacement
     /// caused by losing time-priority due to price change or quantity increase
-    new_order_id: Option<u64>,
+    new_order_id: Option<OrderId>,
     /// The results of the order processing
     order_processing_results: OrderProcessingResults,
 }
@@ -15,11 +16,17 @@ pub struct AmendReport {
 #[allow(unused)]
 impl AmendReport {
     /// Create a new amend report
-    pub(crate) fn new(new_order_id: Option<u64>, amended_order: OrderProcessingResult) -> Self {
+    pub(crate) fn new(amended_order: OrderProcessingResult) -> Self {
         Self {
-            new_order_id,
+            new_order_id: None,
             order_processing_results: OrderProcessingResults::new(amended_order),
         }
+    }
+
+    /// Return this amend report with the new order ID set
+    pub(crate) fn with_new_order_id(mut self, new_order_id: OrderId) -> Self {
+        self.new_order_id = Some(new_order_id);
+        self
     }
 
     /// Return this amend report with the triggered orders set
@@ -34,7 +41,7 @@ impl AmendReport {
 
     /// Get the new order ID, if the amend command resulted in an order replacement
     /// caused by losing time-priority due to price change or quantity increase
-    pub fn new_order_id(&self) -> Option<u64> {
+    pub fn new_order_id(&self) -> Option<OrderId> {
         self.new_order_id
     }
 
@@ -46,5 +53,23 @@ impl AmendReport {
     /// Get the other orders whose state changed as a consequence (e.g., inactive pegged orders becoming active)
     pub fn triggered_orders(&self) -> &[OrderProcessingResult] {
         self.order_processing_results.triggered_orders()
+    }
+}
+
+impl From<OrderProcessingResults> for AmendReport {
+    fn from(results: OrderProcessingResults) -> Self {
+        Self {
+            new_order_id: None,
+            order_processing_results: results,
+        }
+    }
+}
+
+impl From<SubmitReport> for AmendReport {
+    fn from(report: SubmitReport) -> Self {
+        Self {
+            new_order_id: None,
+            order_processing_results: report.into(),
+        }
     }
 }
