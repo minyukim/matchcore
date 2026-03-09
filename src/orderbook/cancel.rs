288909d2d2302a99473c1c887d21191d69322d54
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-09 16:18:36 +0900

    feat: impl limit order cancellation

--- src/orderbook/cancel.rs
+++ src/orderbook/cancel.rs
@@ -17,8 +17,11 @@ impl OrderBook {
     }
 
     /// Cancel a limit order
-    fn cancel_limit_order(&mut self, _order_id: OrderId) -> Result<(), RejectReason> {
-        todo!()
+    fn cancel_limit_order(&mut self, id: OrderId) -> Result<(), RejectReason> {
+        self.remove_limit_order(id)
+            .ok_or(RejectReason::OrderNotFound)?;
+
+        Ok(())
     }
 
     /// Cancel a pegged order
