275d5460f2b8e7422469590be54e09fbbbd8fcb7
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-12 20:51:09 +0900

    feat: impl report display

--- src/report.rs
+++ src/report.rs
@@ -10,7 +10,9 @@ pub use order_outcome::*;
 pub use reasons::*;
 pub use trade::*;
 
-use crate::CommandMeta;
+use crate::{CommandMeta, utils::write_indented};
+
+use std::fmt;
 
 use serde::{Deserialize, Serialize};
 
@@ -37,5 +39,39 @@ impl CommandExecutionReport {
     /// Get the command outcome
     pub fn outcome(&self) -> &CommandOutcome {
         &self.outcome
+    }
+}
+
+impl fmt::Display for CommandExecutionReport {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "report:")?;
+        writeln!(f, "  sequence number: {}", self.meta().sequence_number)?;
+        writeln!(f, "  timestamp: {}", self.meta().timestamp)?;
+        write_indented(f, &self.outcome().to_string(), 2)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests_command_execution_report {
+    use super::*;
+    use crate::{CommandMeta, CommandOutcome, OrderId, OrderOutcome, SequenceNumber, Timestamp};
+
+    #[test]
+    fn test_display() {
+        let command_execution_report = CommandExecutionReport::new(
+            CommandMeta {
+                sequence_number: SequenceNumber(1),
+                timestamp: Timestamp(1000),
+            },
+            CommandOutcome::Applied(AppliedCommand::Submit(CommandEffects::new(
+                OrderOutcome::new(OrderId(1)),
+            ))),
+        );
+        println!("{}", command_execution_report);
+        assert_eq!(
+            command_execution_report.to_string(),
+            "report:\n  sequence number: 1\n  timestamp: 1000\n  submit applied, effects:\n    target order(1):\n      not matched\n      not cancelled\n"
+        );
     }
 }
