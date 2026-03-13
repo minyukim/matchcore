8f4b6471335a6644906b30be68624461a1409a41
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-13 12:20:55 +0900

    feat: pegged order example

---
+++ examples/pegged_orders.rs
@@ -0,0 +1,187 @@
+mod helpers;
+
+use matchcore::*;
+
+fn main() {
+    let mut book = OrderBook::new("ETH/USD");
+
+    // Submit a primary pegged buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Pegged(PeggedOrder::new(
+                PegReference::Primary,
+                Quantity(100),
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    let target_order_id = helpers::target_order_id(&outcome).unwrap();
+
+    // Amend the buy order to a market pegged buy order
+    // The market pegged order will reside at the primary peg level waiting for the new sell order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Amend(AmendCmd {
+            order_id: target_order_id,
+            patch: AmendPatch::Pegged(
+                PeggedOrderPatch::new()
+                    .with_peg_reference(PegReference::Market)
+                    .with_quantity(Quantity(200)),
+            ),
+        }),
+    });
+    println!("{}", outcome);
+
+    let new_target_order_id = helpers::target_order_id(&outcome).unwrap();
+
+    // Submit a standard sell order
+    // The submission command will trigger the market pegged order to be matched with the new sell order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Limit(LimitOrder::new(
+                Price(110),
+                QuantityPolicy::Standard {
+                    quantity: Quantity(100),
+                },
+                OrderFlags::new(Side::Sell, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Cancel the remaining the market pegged buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Cancel(CancelCmd {
+            order_id: new_target_order_id,
+            order_kind: OrderKind::Pegged,
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a mid price pegged buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Pegged(PeggedOrder::new(
+                PegReference::MidPrice,
+                Quantity(100),
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a primary pegged buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Pegged(PeggedOrder::new(
+                PegReference::Primary,
+                Quantity(100),
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a standard buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Limit(LimitOrder::new(
+                Price(100),
+                QuantityPolicy::Standard {
+                    quantity: Quantity(10),
+                },
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a standard sell order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Limit(LimitOrder::new(
+                Price(110),
+                QuantityPolicy::Standard {
+                    quantity: Quantity(10),
+                },
+                OrderFlags::new(Side::Sell, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a market pegged sell order - mid price inactive (spread > 1)
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Market(MarketOrder::new(Quantity(90), Side::Sell, false)),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a standard buy order
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Limit(LimitOrder::new(
+                Price(109),
+                QuantityPolicy::Standard {
+                    quantity: Quantity(10),
+                },
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    });
+    println!("{}", outcome);
+
+    // Submit a market pegged sell order - mid price active (spread <= 1)
+    let outcome = book.execute(&Command {
+        meta: CommandMeta {
+            sequence_number: helpers::sequence_number(),
+            timestamp: helpers::now(),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Market(MarketOrder::new(Quantity(130), Side::Sell, false)),
+        }),
+    });
+    println!("{}", outcome);
+}
