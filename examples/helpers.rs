78450bd5bb18d7ce980ff866b5cba37be64f5e1c
Minhyuk Kim <105956521+minyukim@users.noreply.github.com>
2026-03-13 15:53:43 +0900

    Add basic examples (#82)
    * feat: impl timestamp and sequence number helpers
    
    * feat: impl target order id helper
    
    * feat: standard order example
    
    * feat: market order example
    
    * feat: iceberg order example
    
    * feat: pegged order example
    
    * chore: allow dead code for helpers module
    
    * chore: makefile build examples
    
    * ci: rust-ci build examples

---
+++ examples/helpers.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code)]
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use matchcore::{CommandOutcome, CommandReport, OrderId, SequenceNumber, Timestamp};
+
+/// Helper function to get the current timestamp
+/// In real-world scenarios, the timestamp should be in the input event record.
+pub fn now() -> Timestamp {
+    let duration = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    Timestamp(duration.as_secs())
+}
+
+/// Helper function to get the current sequence number
+/// In real-world scenarios, the sequence number should be the input event record's offset,
+/// e.g., offset of the event stream, or row number of the CSV file.
+pub fn sequence_number() -> SequenceNumber {
+    static mut NEXT_SEQUENCE_NUMBER: SequenceNumber = SequenceNumber(0);
+
+    unsafe {
+        let sequence_number = NEXT_SEQUENCE_NUMBER;
+        NEXT_SEQUENCE_NUMBER = sequence_number.next();
+        sequence_number
+    }
+}
+
+/// Helper function to get the target order ID from the command outcome
+/// Returns `None` if the command was rejected or the order was cancelled
+pub fn target_order_id(outcome: &CommandOutcome) -> Option<OrderId> {
+    match outcome {
+        CommandOutcome::Applied(CommandReport::Submit(command_effects)) => {
+            Some(command_effects.target_order().order_id())
+        }
+        CommandOutcome::Applied(CommandReport::Amend(command_effects)) => {
+            Some(command_effects.target_order().order_id())
+        }
+        _ => None,
+    }
+}
+
+fn main() {}
