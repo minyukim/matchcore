414d99e0dc181912f52b433945f9da9a14ef66d4
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-14 19:57:46 +0900

    feat: bench pegged orders submission

--- benches/submit.rs
+++ benches/submit.rs
@@ -131,5 +131,63 @@ pub fn benches_submit(c: &mut Criterion) {
         })
     });
 
+    let command = Command {
+        meta: CommandMeta {
+            sequence_number: SequenceNumber(0),
+            timestamp: Timestamp(0),
+        },
+        kind: CommandKind::Submit(SubmitCmd {
+            order: NewOrder::Pegged(PeggedOrder::new(
+                PegReference::Primary,
+                Quantity(100),
+                OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+            )),
+        }),
+    };
+    group.bench_function("single_pegged_order_fresh_book", |b| {
+        b.iter(|| {
+            let mut book: OrderBook = OrderBook::new("TEST");
+            let outcome = book.execute(black_box(&command));
+            black_box(outcome);
+        })
+    });
+
+    let commands: Vec<Command> = (0..10_000)
+        .map(|i| {
+            let side = if i % 2 == 0 { Side::Buy } else { Side::Sell };
+            let reference = if i % 3 == 0 {
+                PegReference::Primary
+            } else if i % 3 == 1 {
+                PegReference::Market
+            } else {
+                PegReference::MidPrice
+            };
+
+            Command {
+                meta: CommandMeta {
+                    sequence_number: SequenceNumber(i),
+                    timestamp: Timestamp(i),
+                },
+                kind: CommandKind::Submit(SubmitCmd {
+                    order: NewOrder::Pegged(PeggedOrder::new(
+                        reference,
+                        Quantity(100),
+                        OrderFlags::new(side, false, TimeInForce::Gtc),
+                    )),
+                }),
+            }
+        })
+        .collect();
+    group.bench_function("10k_pegged_orders_fresh_book", |b| {
+        b.iter(|| {
+            let mut book = OrderBook::new("TEST");
+            for command in &commands {
+                let result = book.execute(black_box(command));
+                black_box(result);
+            }
+            black_box(book);
+        })
+    });
+
     group.finish();
 }
