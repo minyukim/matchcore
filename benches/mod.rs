a26202811604c2804aa00ba4c07e5e695c16b48e
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-14 21:16:06 +0900

    feat: add order cancellation benches

--- benches/mod.rs
+++ benches/mod.rs
@@ -1,8 +1,14 @@
 use criterion::{criterion_group, criterion_main};
 
 mod amend;
+mod cancel;
 mod submit;
 
-criterion_group!(benches, submit::benches_submit, amend::benches_amend);
+criterion_group!(
+    benches,
+    submit::benches_submit,
+    amend::benches_amend,
+    cancel::benches_cancel
+);
 
 criterion_main!(benches);
