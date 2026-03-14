4fa3199cab3403ed2cfda4592080dcf3b927ad71
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-14 20:24:59 +0900

    feat: add single_order_quantity_decrease

--- benches/mod.rs
+++ benches/mod.rs
@@ -1,7 +1,8 @@
 use criterion::{criterion_group, criterion_main};
 
+mod amend;
 mod submit;
 
-criterion_group!(benches, submit::benches_submit);
+criterion_group!(benches, submit::benches_submit, amend::benches_amend);
 
 criterion_main!(benches);
