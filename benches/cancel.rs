edc2e96a11c7b74139ea5f709a2d60c2c1175b96
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-22 16:58:03 +0900

    chore: add multi-level cancel benches

--- benches/cancel.rs
+++ benches/cancel.rs
@@ -33,6 +33,27 @@ pub fn benches_cancel(c: &mut Criterion) {
         )
     });
 
+    let command = Command {
+        meta: CommandMeta {
+            sequence_number: SequenceNumber(n_orders),
+            timestamp: Timestamp(n_orders),
+        },
+        kind: CommandKind::Cancel(CancelCmd {
+            order_id: OrderId(n_orders / 2),
+            order_kind: OrderKind::Limit,
+        }),
+    };
+    group.bench_function("single_order_in_multi_level_book_cancel", |b| {
+        b.iter_batched(
+            || build_multi_level_book(n_orders),
+            |mut book| {
+                let outcome = book.execute(black_box(&command));
+                black_box(outcome);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
     let commands: Vec<Command> = (0..n_orders)
         .map(|i| Command {
             meta: CommandMeta {
@@ -48,6 +69,18 @@ pub fn benches_cancel(c: &mut Criterion) {
     group.bench_function("10k_orders_in_single_level_book_cancel", |b| {
         b.iter_batched(
             || build_single_level_book(n_orders),
+            |mut book| {
+                for command in &commands {
+                    let outcome = book.execute(black_box(command));
+                    black_box(outcome);
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+    group.bench_function("10k_orders_in_multi_level_book_cancel", |b| {
+        b.iter_batched(
+            || build_multi_level_book(n_orders),
             |mut book| {
                 for command in &commands {
                     let outcome = book.execute(black_box(command));
@@ -74,6 +107,31 @@ fn build_single_level_book(n_orders: u64) -> OrderBook {
             kind: CommandKind::Submit(SubmitCmd {
                 order: NewOrder::Limit(LimitOrder::new(
                     Price(100),
+                    QuantityPolicy::Standard {
+                        quantity: Quantity(100),
+                    },
+                    OrderFlags::new(Side::Buy, false, TimeInForce::Gtc),
+                )),
+            }),
+        });
+    }
+
+    book
+}
+
+/// Helper function to build an order book with `n_orders` standard orders in multiple levels
+fn build_multi_level_book(n_orders: u64) -> OrderBook {
+    let mut book = OrderBook::new("TEST");
+
+    for i in 0..n_orders {
+        book.execute(&Command {
+            meta: CommandMeta {
+                sequence_number: SequenceNumber(i),
+                timestamp: Timestamp(i),
+            },
+            kind: CommandKind::Submit(SubmitCmd {
+                order: NewOrder::Limit(LimitOrder::new(
+                    Price(100 + (i % 10)),
                     QuantityPolicy::Standard {
                         quantity: Quantity(100),
                     },
