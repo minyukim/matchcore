427397dd439a771b28b429475e32fbe5f41fa3ee
Minhyuk Kim <kmin1318.dev@gmail.com>
2026-03-24 20:44:38 +0900

    refactor: reorder book fields

--- src/orderbook/book/pegged_book.rs
+++ src/orderbook/book/pegged_book.rs
@@ -6,18 +6,18 @@ use std::{
     collections::{BinaryHeap, HashMap},
 };
 
-/// Pegged order book that manages pegged orders and peg levels.
+/// Pegged order book that manages pegged orders and peg levels
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, Default)]
 pub struct PeggedBook {
+    /// Pegged orders indexed by order ID for O(1) lookup
+    pub(crate) orders: HashMap<OrderId, RestingPeggedOrder>,
+
     /// Pegged bid side levels, one for each reference price type
     pub(crate) bid_levels: [PegLevel; PegReference::COUNT],
 
     /// Pegged ask side levels, one for each reference price type
     pub(crate) ask_levels: [PegLevel; PegReference::COUNT],
-
-    /// Pegged orders indexed by order ID for O(1) lookup
-    pub(crate) orders: HashMap<OrderId, RestingPeggedOrder>,
 
     /// Queue of pegged order IDs to be expired, stored in a min heap of tuples of
     /// (expires_at, order_id) with O(log N) push and pop
@@ -30,6 +30,11 @@ impl PeggedBook {
         Self::default()
     }
 
+    /// Get the pegged orders indexed by order ID
+    pub fn orders(&self) -> &HashMap<OrderId, RestingPeggedOrder> {
+        &self.orders
+    }
+
     /// Get the pegged bid side levels
     pub fn bid_levels(&self) -> &[PegLevel; PegReference::COUNT] {
         &self.bid_levels
@@ -38,11 +43,6 @@ impl PeggedBook {
     /// Get the pegged ask side levels
     pub fn ask_levels(&self) -> &[PegLevel; PegReference::COUNT] {
         &self.ask_levels
-    }
-
-    /// Get the pegged orders indexed by order ID
-    pub fn orders(&self) -> &HashMap<OrderId, RestingPeggedOrder> {
-        &self.orders
     }
 
     /// Get the queue of pegged order IDs to be expired
